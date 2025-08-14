0b1842565dafc40409887f67ad9705b20b42fe3e
Shaina <shainapereira37@gmail.com>
2025-08-14 11:19:43 +0530

    Delete Personal_Transaction_app directory

--- Personal_Transaction_app/android_client/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        requestSmsPermission()
-    }
-}
-if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-    requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1001)
-}
--- Personal_Transaction_app/android_client/Permissions.kt
+++
@@ -1,7 +0,0 @@
-fun Activity.requestSmsPermission() {
-    ActivityCompat.requestPermissions(
-        this,
-        arrayOf(Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS),
-        1001
-    )
-}
